bb9ae80bec18c3ebb8ee0e6a80284fb23d215fe9
ChinnuS7401 <chinnusandeep27@gmail.com>
2025-10-14 16:18:15 +0530

    Update book.c
    for loop added

--- C Programming/book.c
+++ C Programming/book.c
@@ -1,19 +1,33 @@
-#include <stdio.h>
-
-typedef struct {
-    char name[50];
+#include<stdio.h>
+#include<string.h>
+ 
+typedef struct{
+    char title[50];
     char author[50];
     float price;
-} books;
-
-int main() {
-    books b1 = {"Randamoozham", "M.T Vasudevan Nair", 575.00};
-    books b2 = {"The Goat Life", "Benyamin", 300.00};
-    books b3 = {"Paranettu", "Rahul Kartha", 152.00};
-
-    printf("name=%s, author=%s, price=%.2f\n", b1.name, b1.author, b1.price);
-    printf("name=%s, author=%s, price=%.2f\n", b2.name, b2.author, b2.price);
-    printf("name=%s, author=%s, price=%.2f\n", b3.name, b3.author, b3.price);
-
+}books;
+ 
+int main(){
+    books book[3];
+    int i,maxprice=0,minprice=0;
+    for(i=0;i<3;i++){
+        printf("Enter the details of book %d\n",i+1);
+        printf("title:");
+        scanf("%s",book[i].title);
+        printf("author=");
+        scanf("%s",book[i].author);
+        printf("price:");
+        scanf("%f",&book[i].price);
+    }
+    for(i=0;i<3;i++){
+        if(book[i].price>book[maxprice].price)
+        maxprice=i;
+        if(book[i].price<book[minprice].price)
+        minprice=i;
+    }
+    printf("Expensive book is=\n");
+    printf("title:%s\nauthor=%s\nprice=%f\n",book[maxprice].title,book[maxprice].author,book[maxprice].price);
+    printf("least expensive book is=\n");
+    printf("title:%s\nauthor=%s\nprice=%f\n",book[minprice].title,book[minprice].author,book[minprice].price);
     return 0;
 }
